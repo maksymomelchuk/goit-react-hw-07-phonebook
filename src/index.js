94698bc959217e64e598529b60a853a8f0f8a611
maksymomelchuk <omelchukmaksym@gmail.com>
2022-12-08 13:00:19 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import axios from 'axios';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -15,3 +16,13 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </Provider>
   </React.StrictMode>
 );
+
+axios.defaults.baseURL = 'https://6390a05465ff41831119c8b1.mockapi.io';
+
+async function fetch() {
+  const response = await axios.get('/contacts');
+  const data = response.data;
+  console.log(data);
+}
+
+fetch();
